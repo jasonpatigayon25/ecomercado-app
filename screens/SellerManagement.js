fdb88e3c1fed1f9aa4307b1dda76e6166cd3a4e6
Markymy <rocabertemarky09@gmail.com>
2024-03-31 21:09:03 +0800

    ECOMERCADO

--- screens/SellerManagement.js
+++ screens/SellerManagement.js
@@ -4,26 +4,35 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { getAuth } from 'firebase/auth';
 import { db } from '../config/firebase';
 import { collection, query, where, onSnapshot } from 'firebase/firestore';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const SellerManagement = ({ navigation }) => {
-    const animation = useRef(new Animated.Value(0)).current;
-    const [isSeller, setIsSeller] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const auth = getAuth();
-    const user = auth.currentUser;
+  const animation = useRef(new Animated.Value(0)).current;
+  const [isSeller, setIsSeller] = useState(false);
+  const [sellerInfo, setSellerInfo] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const auth = getAuth();
+  const user = auth.currentUser;
 
-    useEffect(() => {
+  useEffect(() => {
+    if (user) {
         setIsLoading(true);
-        if (user) {
-            const sellerQuery = query(collection(db, 'registeredSeller'), where('email', '==', user.email));
-            const unsubscribe = onSnapshot(sellerQuery, (querySnapshot) => {
-                setIsSeller(!querySnapshot.empty);
+        const unsubscribeSellerInfo = onSnapshot(
+            query(collection(db, 'registeredSeller'), where('email', '==', user.email)),
+            (querySnapshot) => {
+                const sellerData = querySnapshot.docs.map(doc => doc.data())[0]; 
+                if (sellerData) {
+                    setSellerInfo(sellerData);
+                    setIsSeller(true);
+                } else {
+                    setIsSeller(false);
+                }
                 setIsLoading(false);
-            });
-
-            return () => unsubscribe();
-        }
-    }, [user]);
+            }
+        );
+        return () => unsubscribeSellerInfo();
+    }
+}, [user]);
 
     useEffect(() => {
         Animated.loop(
@@ -59,6 +68,24 @@ const SellerManagement = ({ navigation }) => {
         );
     }
 
+    const SellerInfoView = () => (
+      sellerInfo && (
+          <View style={styles.sellerInfoContainer}>
+              <Text style={styles.sellerName}>{sellerInfo.sellerName}</Text>
+              <Text style={styles.fullName}>{sellerInfo.registeredName}</Text>
+              <View style={styles.infoRow}>
+                  <MaterialIcons name="email" size={20} color="#05652D" />
+                  <Text style={styles.subText}>{sellerInfo.email}</Text>
+              </View>
+              <View style={styles.infoRow}>
+                  <MaterialIcons name="place" size={20} color="#05652D" />
+                  <Text style={styles.miniSubText}>{sellerInfo.pickupAddress}</Text>
+              </View>
+          </View>
+      )
+  );
+
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -67,6 +94,7 @@ const SellerManagement = ({ navigation }) => {
                 </TouchableOpacity>
                 <Text style={styles.title}>Seller Management</Text>
             </View>
+            <SellerInfoView />
             <ScrollView style={styles.scrollView}>
                 {isSeller ? (
                     <View style={styles.transactionsRow}>
@@ -183,6 +211,44 @@ const styles = StyleSheet.create({
         color: '#FFFFFF',
         fontSize: 20,
         fontWeight: 'bold',
+    },
+    sellerInfoContainer: {
+      backgroundColor: '#FFFFFF',
+      paddingVertical: 20,
+      paddingHorizontal: 15,
+      borderRadius: 10,
+      margin: 20,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.22,
+      shadowRadius: 2.22,
+      elevation: 3,
+    },
+    sellerName: {
+      fontSize: 22,
+      fontWeight: 'bold',
+      color: '#05652D',
+      marginBottom: 4,
+    },
+    fullName: {
+      fontSize: 18,
+      color: '#05652D',
+      marginBottom: 4,
+    },
+    infoRow: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: 5,
+    },
+    subText: {
+      fontSize: 16,
+      color: '#05652D',
+      marginLeft: 8,
+    },
+    miniSubText: {
+      fontSize: 14,
+      color: '#05652D',
+      marginLeft: 8,
     },
 });
 
