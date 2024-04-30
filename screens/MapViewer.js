005141371eacddbc07853af20377e8abd7ea783d
Markymy <rocabertemarky09@gmail.com>
2024-04-30 08:14:07 +0800

    ECOMERCADO

--- screens/MapViewer.js
+++ screens/MapViewer.js
@@ -1,54 +1,52 @@
-// import React from 'react';
-// import { WebView } from 'react-native-webview';
-
-// const MapViewer = () => {
-//   return (
-//     <WebView
-//       source={{
-//         uri: 'https://www.google.com/maps/@?AIzaSyA6bqssrv5NTEf2lr6aZMSh_4hGrnjr32g=1&map_action=map&center=10.3157,123.8854&zoom=10', 
-//       }}
-//       style={{ flex: 1 }}
-//       javaScriptEnabled={true}
-//       domStorageEnabled={true}
-//     />
-//   );
-// };
-
-// export default MapViewer;
-
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { WebView } from 'react-native-webview';
 
 const MapViewer = () => {
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <style type="text/css">
+        #map {
+          height: 100%;
+        }
+        html, body {
+          height: 100%;
+          margin: 0;
+          padding: 0;
+        }
+      </style>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA6bqssrv5NTEf2lr6aZMSh_4hGrnjr32g"></script>
+      <script>
+        function initMap() {
+          var cebu = {lat: 10.3157, lng: 123.8854};
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: cebu
+          });
+          var marker = new google.maps.Marker({
+            position: cebu,
+            map: map,
+            title: 'Cebu City'
+          });
+        }
+      </script>
+    </head>
+    <body onload="initMap()">
+      <div id="map"></div>
+    </body>
+    </html>
+  `;
+
   return (
-    <MapView
-      style={styles.map}
-      initialRegion={{
-        latitude: 10.3157,
-        longitude: 123.8854,
-        latitudeDelta: 0.1,
-        longitudeDelta: 0.1,
-      }}
-    >
-      <Marker
-        coordinate={{ latitude: 10.3157, longitude: 123.8854 }}
-        title="Cebu City"
-        description="The Queen City of the South"
-      />
-    </MapView>
+    <WebView
+      originWhitelist={['*']}
+      source={{ html: htmlContent }}
+      style={{ flex: 1 }}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    backgroundColor: 'transparent',
-  },
-  map: {
-    flex: 1,
-  },
-});
-
 
 export default MapViewer;
