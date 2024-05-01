a6c0be57f45ea1be0abc215581d781d86a700f7d
Markymy <rocabertemarky09@gmail.com>
2024-05-01 10:14:42 +0800

    ECOMERCADO

--- screens/Home.js
+++ screens/Home.js
@@ -532,7 +532,7 @@ const Home = ({ navigation, route }) => {
         </View>
         <View style={[styles.donationsContainer, styles.sectionContainer]}>
                 <View style={styles.donationsHeader}>
-                    <Text style={styles.sectionTitle}>Recent Donations for Users</Text>
+                    <Text style={styles.sectionTitle}>Donations You Can Request</Text>
                     <TouchableOpacity onPress={shuffleDonations} style={styles.shuffleButton}>
                         <Icon name="random" size={20} color="#05652D" />
                     </TouchableOpacity>
