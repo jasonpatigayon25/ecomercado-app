44b836a3f9d0d541571eee8861d3750e5a2e397d
Markymy <rocabertemarky09@gmail.com>
2024-05-01 15:53:41 +0800

    ECOMERCADO

--- screens/Home.js
+++ screens/Home.js
@@ -1217,7 +1217,7 @@ searchSuggestions: {
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
-    elevation: 5,
+
   },
   morePhotosText: {
     color: '#FFF', 
